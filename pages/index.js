5e7a0a8cd6d38b5e00aee2dc70c49d71a8f6da76
shani barak <shanibrk@gmail.com>
2022-05-14 19:56:35 +0300

    adding home component, footer and banner

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
 import React from 'react'
+import { Product, FooterBanner, HeroBanner } from '../components'
+import { client } from '../lib/client'
 
-const Home = () => {
-  return (
+const Home = ({products, bannerData}) => (
+  
     <>
-    HeroBanner
-    <div>
+    <HeroBanner HeroBanner={bannerData.length && bannerData[0]}/>
+    <div className='products-heading'>
       <h2>Best Selling Products</h2>
     </div>
 
-    <div>
-      {['Product1', 'Product2'].map((product)=>product)}
+    <div className='products-container'>
+      {products.map((product)=> <Product key={product._id} product={product}/>)}
     </div>
 
-    Footer
+    <FooterBanner FooterBanner={bannerData && bannerData[0]}/>
     </>
 
   )
-}
+ 
+
+ export const getServerSideProps= async()=>{
+   const query= '*[_type == "product"]'
+   const products= await client.fetch(query)
+
+   const bannerQuery= '*[_type == "banner"]'
+   const bannerData= await client.fetch(bannerQuery)
+   return{
+     props:{products, bannerData}
+   }
+    
+  }
 
 export default Home
