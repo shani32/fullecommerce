3184879f13df46941c1ff4be44ab0bfdc4aa6dd2
shani barak <shanibrk@gmail.com>
2022-05-17 15:12:05 +0300

    adding the basic of stripe payments

---
+++ pages/api/Sripe.js
@@ -0,0 +1,38 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+      console.log(req.body.cartItems)
+    try {
+        const params= {
+            submit_type:'pay',
+            mode:'payment',
+            payment_metgod_types:['card'],
+            billing_addres_collection: 'auto',
+            shipping_options:[
+                {shipping_rate: 'shr_1L0O3NB3DS1MblxCxxQbF97Q'},
+                {shipping_rate: 'shr_1L0O5qB3DS1MblxCYuq9mAQp'},
+
+            ],
+            line_items: [
+              {
+                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                price: '{{PRICE_ID}}',
+                quantity: 1,
+              },
+            ],
+            mode: 'payment',
+            success_url: `${req.headers.origin}/?success=true`,
+            cancel_url: `${req.headers.origin}/?canceled=true`,
+          }
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
